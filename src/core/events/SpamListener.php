ebfdf8f58cf1fed50dc0040217f4d16f7a2d00f9
zOmAr <64379283+zOmArRD@users.noreply.github.com>
2020-11-23 11:05:27 -0400

    Code Optimized

--- src/core/events/SpamListener.php
+++ src/core/events/SpamListener.php
@@ -9,10 +9,13 @@ use core\Main;
 
 class SpamListener implements Listener{
 
-    public $ips = [".es", ".net", ".ddns", ".eu", ".us", ".club", ".sytes", ".cf", ".tk", ".ml", ".pro", ".com", ".1", ".2", ".3", ".4", ".5", ".6", ".7", ".8", ".9", ".10", ",net", ",pro", ",com", ",ml", ",tk", ",cf", "cubecraft", "versai"];
-    public $allowedips = ["play.endgames.cf", "shop.endgames.cf", "endgames.cf"];
+    public array $ips = [".es", ".net", ".ddns", ".eu", ".us", ".club", ".sytes", ".cf", ".tk", ".ml", ".pro", ".com", ".1", ".2", ".3", ".4", ".5", ".6", ".7", ".8", ".9", ".10", ",net", ",pro", ",com", ",ml", ",tk", ",cf", "cubecraft", "versai"];
+    public array $allowed = ["play.endgames.cf", "shop.endgames.cf", "endgames.cf"];
 
-    public function antiSpam(PlayerChatEvent $ev)
+    /**
+     * @param PlayerChatEvent $ev
+     */
+    public function PlayerChatEvent(PlayerChatEvent $ev)
     {
         $p = $ev->getPlayer();
         $pn = $p->getName();
@@ -21,24 +24,22 @@ class SpamListener implements Listener{
         foreach ($this->ips as $ips) {
             if (strpos($msg, $ips)) {
                 if ($p->hasPermission("endgames.staff")) {
-                    //Code
+                    $ev->setCancelled(false);
                 } else {
-                    $p->sendMessage($pref.  "§cWe have seen that you tried to pass ip from another server, the staff will take serious measures if this happens again");
+                    $p->sendMessage($pref . "§cWe have seen that you tried to pass ip from another server, the staff will take serious measures if this happens again");
                     $ev->setCancelled(true);
                 }
                 foreach (Main::getInstance()->getServer()->getOnlinePlayers() as $player) {
                     if ($player->hasPermission("endgames.staff")) {
-                        $player->sendMessage($pref. "§cAttention, the player §6$pn has tried to pass an IP\n§bMessage: §c$msg");
+                        $player->sendMessage($pref . "§cAttention, the player §6$pn has tried to pass an IP\n§bMessage: §c$msg");
                     }
                 }
-                foreach ($this->allowedips as $allow) {
+                foreach ($this->allowed as $allow) {
                     if (strpos($msg, $allow)) {
                         $ev->setCancelled(false);
                     }
-
                 }
             }
         }
-
     }
 }
